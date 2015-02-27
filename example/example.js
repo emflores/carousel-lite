8b5288180e815c6b2315e8db8c3f8d67f90d56ff
emflores <flores.michael@gmail.com>
2015-02-27 08:50:52 -0800

    added an example

---
+++ example/example.js
@@ -0,0 +1,10 @@
+var carousel = require( 'carousel-lite' );
+
+window.addEventListener( 'DOMContentLoaded', function () {
+    carousel.register({
+        carousel: '.list',
+        items: '.item',
+        previous: '.previous',
+        next: '.next'
+    });
+});
