9eb1cccb847099cdbb403461ac18c0306486ea71
emflores <flores.michael@gmail.com>
2015-02-27 12:35:03 -0800

    added doc block for entry point

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ function bindHandlers ( carousel, items, next, previous ) {
     });
 }
 
+/**
+ * Adds carousel next/previous handlers to a carousel
+ * @param  {object}   args
+ * @param  {string}   args.carousel     Selector for carousel ul
+ * @param  {string}   args.items        Selector for li children of carousel ul
+ * @param  {string}   args.next         Selector for next button
+ * @param  {string}   args.previous     Selector for previous button
+ */
 module.exports.register = function ( args ) {
     var carousel = win.document.querySelector( args.carousel );
     var next     = win.document.querySelector( args.next );
