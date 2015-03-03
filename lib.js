a32ef09d1cd2a58750f3323e85a4ec1054e8b8b7
emflores <flores.michael@gmail.com>
2015-03-02 19:48:33 -0800

    added doc blocks to lib

--- lib.js
+++ lib.js
@@ -2,6 +2,23 @@ var DISABLED = 'carousel-button-disabled';
 
 var _findIndex = require( 'lodash-compat/array/findIndex' );
 
+/**
+ * Returns max leftOffset for the carousel
+ * @param  {object} carousel
+ * @return {number}
+ */
+function getMaxScroll ( carousel ) {
+    return carousel.scrollWidth - carousel.clientWidth;
+}
+
+/**
+ * Returns an object that describes whether or not the next/previous buttons
+ * should be disabled.
+ * @param  {integer} newIndex Index position of the new (just scrolled to) item
+ * @param  {number}  newPos   Left offset of the new item
+ * @param  {object}  carousel
+ * @return {object}
+ */
 function isDisabled ( newIndex, newPos, carousel ) {
     return {
         previous: newIndex === 0,
@@ -26,31 +43,58 @@ function getScrollIndex ( carousel, items ) {
     });
 }
 
-function getMaxScroll ( carousel ) {
-    return carousel.scrollWidth - carousel.clientWidth;
-}
-
-function getNextPosition ( carousel, newItem ) {
+/**
+ * Returns the left offset of the new item relative to the
+ * left offset of the carousel.
+ * @param   {object} carousel
+ * @param   {object} newItem
+ * @return  {number}
+ */
+function getNewPosition ( carousel, newItem ) {
     return newItem.offsetLeft - carousel.offsetLeft;
 }
 
+/**
+ * Calculates the new left offset of the carousel, and enables/disables
+ * the next/previous buttons accordingly.
+ * @param  {object}   carousel
+ * @param  {array}    items
+ * @param  {integer}  newIndex
+ * @param  {object}   next
+ * @param  {object}   previous
+ * @return {number}
+ */
 function syncPositionState ( carousel, items, newIndex, next, previous ) {
     var newItem  = items[ newIndex ];
-    var nextPos  = getNextPosition( carousel, newItem );
-    var disabled = isDisabled( newIndex, nextPos, carousel );
+    var newPos   = getNewPosition( carousel, newItem );
+    var disabled = isDisabled( newIndex, newPos, carousel );
 
     toggleDisabled( previous, disabled.previous );
     toggleDisabled( next, disabled.next );
 
-    return nextPos;
+    return newPos;
 }
 
+/**
+ * Whether or not the carousel has items outside the visible area.
+ * @param   {object} carousel
+ * @param   {array}  items
+ * @return  {boolean}
+ */
 function carouselCanScroll ( carousel, items ) {
     var lastItem            = items[ items.length - 1 ];
     var lastItemRightOffset = lastItem.offsetLeft - carousel.offsetLeft + lastItem.offsetWidth;
     return lastItemRightOffset <= carousel.clientWidth;
 }
 
+/**
+ * Upon use of native scroll, estimate the carousel index position and sync
+ * the enabled/disabled state for the next/previous buttons.
+ * @param {object}   carousel
+ * @param {array}    items
+ * @param {object}   next
+ * @param {object}   previous
+ */
 function handleScroll ( carousel, items, next, previous ) {
     var newIndex = Math.max(
         getScrollIndex( carousel, items ) - 1,
@@ -62,6 +106,11 @@ function handleScroll ( carousel, items, next, previous ) {
     carousel.currentIndex = newIndex;
 }
 
+/**
+ * Toggles the disabled class and attribute
+ * @param {object}  el            DOM element
+ * @param {boolean} shouldDisable
+ */
 function toggleDisabled ( el, shouldDisable ) {
     el.classList.toggle( DISABLED, shouldDisable );
     el.disabled = shouldDisable;
